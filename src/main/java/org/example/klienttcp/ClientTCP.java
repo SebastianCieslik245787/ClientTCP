c73f884ab87e0f3c606f3dfbff0bd2245f53da8d
Sebastian <245787@edu.p.lodz.pl>
2025-04-20 16:53:14 +0200

    Client TCP Programowanie Sieciowe

---
+++ src/main/java/org/example/klienttcp/ClientTCP.java
@@ -0,0 +1,25 @@
+package org.example.klienttcp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ClientTCP extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ClientTCP.class.getResource("ClientWindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 700);
+        stage.setTitle("Client TCP!");
+        stage.setScene(scene);
+        stage.show();
+        ClientWindowController controller = fxmlLoader.getController();
+        controller.initialize();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/org/example/klienttcp/ClientWindowController.java
@@ -0,0 +1,192 @@
+package org.example.klienttcp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.paint.Paint;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/*
+ * Do napisania Clienta TCP wzorowałem się stroną https://systembash.com/a-simple-java-tcp-server-and-tcp-client/
+ */
+
+public class ClientWindowController {
+    @FXML
+    public TextArea addressField;
+    @FXML
+    public TextArea portField;
+    @FXML
+    public TextArea clientLogsField;
+    @FXML
+    public TextArea clientMessageField;
+    @FXML
+    public Button sendButton;
+    @FXML
+    public Button connectButton;
+    @FXML
+    public Button disconnectButton;
+    @FXML
+    public Label errorLabel;
+    @FXML
+    public Label serverStatus;
+
+    private Socket clientSocket = null;
+    private DataOutputStream outToServer = null;
+    private BufferedReader inFromServer = null;
+
+    private boolean isConnected = false;
+
+    public void initialize() {
+        sendButton.setOnAction(_ -> {
+            if(isConnected) {
+                if (validateInputMessage()) {
+                    String message = clientMessageField.getText();
+                    try {
+                        outToServer.writeBytes(message + '\n');
+                        clientMessageField.clear();
+                        logMessage("[" + getTime() + "] Message: \"" + message + "\" has send to server. (" + message.getBytes().length + " bytes)");
+                    } catch (IOException e) {
+                        logMessage("[" + getTime() + "] Can not send message to server");
+                        isConnected = false;
+                        setConnectionToServerStatus(false);
+                    }
+                    try {
+                        logMessage("[" + getTime() + "] Message \"" + inFromServer.readLine() + "\" received from the server. (" + message.getBytes().length + " bytes)");
+                    } catch (IOException e) {
+                        logMessage("[" + getTime() + "] Can not receive message from server");
+                        isConnected = false;
+                        setConnectionToServerStatus(false);
+                    }
+                }
+            }
+            else {
+                logMessage("[" + getTime() + "] Can not send message to server because you are not connected!");
+            }
+        });
+
+        connectButton.setOnAction(_ -> {
+            if(validatePort()){
+                if(validateAddress()){
+                    try {
+                        clientSocket = new Socket(addressField.getText(), Integer.parseInt(portField.getText()));
+                        outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                        inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        isConnected = true;
+                        logMessage("[" + getTime() + "] Connected to server.");
+                        toggleConnectionUI(true);
+                        setConnectionToServerStatus(true);
+                    } catch (IOException e) {
+                        logMessage("[" + getTime() + "] Could not connect to server.");
+                    }
+                }
+            }
+        });
+
+        disconnectButton.setOnAction(_ -> {
+            try {
+                clientSocket.close();
+                isConnected = false;
+                logMessage("[" + getTime() + "] Disconnected from server.");
+                toggleConnectionUI(false);
+                setConnectionToServerStatus(false);
+            } catch (IOException e) {
+                logMessage("[" + getTime() + "] Could not disconnect from server.");
+            }
+        });
+    }
+
+    /*
+     * Do uzyskania aktualnego czasu inspirowałem się https://www.w3schools.com/java/java_date.asp
+     */
+    private String getTime() {
+        LocalDateTime myDateObj = LocalDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return myDateObj.format(myFormatObj);
+    }
+
+    private void logMessage(String message) {
+        clientLogsField.appendText(message + "\n");
+    }
+
+    private void toggleConnectionUI(Boolean connectState) {
+        portField.setEditable(!connectState);
+        addressField.setEditable(!connectState);
+
+        connectButton.setDisable(connectState);
+        connectButton.setVisible(!connectState);
+
+        disconnectButton.setDisable(!connectState);
+        disconnectButton.setVisible(connectState);
+    }
+
+    private Boolean validateInputMessage() {
+        int messageSize = clientMessageField.getText().getBytes().length;
+        if (messageSize == 0) {
+            logMessage("[" + getTime() + "] Can not send empty message to server!");
+            return false;
+        } else if (messageSize > 1024) {
+            logMessage("[" + getTime() + "] Can not send message to server! Message is too large. (Current size: " + messageSize + " bytes; Max size: 1024 bytes)");
+            return false;
+        }
+        return true;
+    }
+
+    /*
+     * Do testowaania i tworzenia regex'ów posłużyłem się https://regexr.com/
+     */
+    private Boolean validatePort(){
+        if(portField.getText().isEmpty()) {
+            errorLabel.setText("Port can not be empty");
+            errorLabel.setVisible(true);
+            return false;
+        }
+
+        String PORT_PATTERN = "^(6553[0-5]|655[0-2]\\d|65[0-4]\\d\\d|6[0-4]\\d\\d\\d|[0-5]?\\d?\\d?\\d?\\d)$";
+
+        if (!portField.getText().matches(PORT_PATTERN)) {
+            errorLabel.setText("Port must be a valid port number! (Possible ports are: 0-65535)");
+            errorLabel.setVisible(true);
+            return false;
+        }
+        errorLabel.setVisible(false);
+        return true;
+    }
+
+    private Boolean validateAddress(){
+        if(addressField.getText().isEmpty()) {
+            errorLabel.setText("Address can not be empty");
+            errorLabel.setVisible(true);
+            return false;
+        }
+
+        String ADDRESS_PATTERN = "^(localhost|((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d\\.){3}.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)))$";
+
+        if(!addressField.getText().matches(ADDRESS_PATTERN)) {
+            errorLabel.setText("Address must be a valid IP address! (Possible addresses are: [0-255].[0-255].[0-255].[0-255] or localhost)");
+            errorLabel.setVisible(true);
+            return false;
+        }
+
+        errorLabel.setVisible(false);
+        return true;
+    }
+
+    private void setConnectionToServerStatus(boolean connectionStatus) {
+        if(connectionStatus) {
+            serverStatus.setText("Connected");
+            serverStatus.setTextFill(Paint.valueOf("#058e2e"));
+        }
+        else {
+            serverStatus.setText("Not Connected");
+            serverStatus.setTextFill(Paint.valueOf("#e10202"));
+        }
+    }
+}
